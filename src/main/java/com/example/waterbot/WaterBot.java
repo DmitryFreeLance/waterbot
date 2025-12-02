b8d0a1fc49046087f83ac338c9c481e82aed4e49
dmitry <dmitryshagaev@bk.ru>
2025-12-02 15:33:50 +0700

    done X9

--- src/main/java/com/example/waterbot/WaterBot.java
+++ src/main/java/com/example/waterbot/WaterBot.java
@@ -331,9 +331,6 @@ public class WaterBot extends TelegramLongPollingBot {
         rows.add(singleButtonRow("📊 Анкета по здоровью", CB_HEALTH_FORM));
         rows.add(singleButtonRow("📞 Записаться на консультацию", CB_CONSULTATION));
 
-        // новая последняя кнопка раздела
-        rows.add(singleButtonRow("Полное очищение организма", CB_FULL_CLEANSE));
-
         // последняя кнопка – переход в Telegram-канал
         InlineKeyboardButton channelButton = new InlineKeyboardButton();
         channelButton.setText("Мой TELEGRAM канал");
